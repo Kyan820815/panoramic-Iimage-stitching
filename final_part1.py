205fb1eaf349635418694f0f82e26dde9d0c31bd
zyyturbo <60275185+zyyturbo@users.noreply.github.com>
2020-04-23 16:33:24 -0400

    Add files via upload
    Part I:  Extract ORB features from all n images. (Because there is no attribute of "SIFT" in the current version of OpenCV, we will use orb features instead. )

---
+++ final_part1.py
@@ -0,0 +1,52 @@
+from skimage import io, color
+import os
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+class point:
+    def __init__(self, pos, orb, knn_list):
+        self.pos = pos # position information of key point
+        self.orb = orb # orb feature
+        self.knn_list = knn_list
+
+
+def load_data(path):
+    files = os.listdir(path)
+    images = []
+    for file in files:
+        images.append(color.rgb2grey(io.imread(path + '/' + file)))
+
+    return np.array(images)
+
+def get_ORB_feature(images):
+    orb = cv2.ORB_create()
+    dic = {}
+    for i in range(len(images)):
+        # get orb features of each image
+        kp, des = orb.detectAndCompute(images[i], None)
+
+        # draw key points if you want
+        img = cv2.drawKeypoints(images[i], kp, np.array([]))
+        plt.imshow(img)
+        plt.show()
+
+        # store each point's information in one list and map to the image
+        pts = []
+        for j in range(len(kp)):
+            pt = point(kp[j], des[j], [])
+            pts.append(pt)
+        dic[i] = pts
+    return dic
+
+
+images = load_data("data")
+dic = get_ORB_feature(images)
+
+
+
+
+
+
+
+
